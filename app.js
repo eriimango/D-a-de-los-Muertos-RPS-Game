48fc33f9a6105c8c6e510c63639c5aa6fa55940e
Ericka <64378078+eriimango@users.noreply.github.com>
2021-11-03 02:44:10 -0400

    update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let userScore = 0;
 let computerScore = 0;
-const userScore_span = document.getElementById("user");
-const computerScore_span = document.getElementById("computer");
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
 const moveDescription_p = document.querySelector(".move-description p");
 const sarape_div = document.getElementById("sarape");
@@ -39,7 +39,6 @@ function lose(userWeapon, computerWeapon) {
     userScore_span.innerHTML = userScore;
     computerScore_span.innerHTML= computerScore;
     moveDescription_p.innerHTML= '${weapons(computerWeapon)}${computerLabel} beats ${weapons(userWeapon)} ${userLabel}. The computer scores.';
-
 }
 
 function draw(userWeapon, computerWeapon) {
@@ -50,10 +49,9 @@ function draw(userWeapon, computerWeapon) {
     userScore_span.innerHTML = userScore;
     computerScore_span.innerHTML= computerScore;
     moveDescription_p.innerHTML= '${weapons(userWeapon)}${userLabel} is the same as ${weapons(computerWeapon)} ${computerLabel}. Its a draw.';
-
 }
 
-functiongame(userWeapon) {
+function game(userWeapon) {
     const computerWeapon = getComputerWeapon();
     switch(userWeapon + computerWeapon) {
         case "skullvidrio":
@@ -70,8 +68,7 @@ functiongame(userWeapon) {
         case "sarapesarape":
         case "vidriovidrio":
             draw(userWeapon, computerWeapon);
-            break;
-            
+            break;        
     }
 }
 
